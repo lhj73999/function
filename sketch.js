6d912055814ce7db12bd95a7be88f0a184a890c4
lhj73999 <ehgkfdl@naver.com>
2014-10-16 15:50:13 +0900

    ddd

--- sketch.js
+++ sketch.js
@@ -9,6 +9,12 @@ function draw()
 {   background(0);
 	fill(255);
 	ellipse(mouseX, mouseY, 100, 100);
+
+//doughnut #1
+fill(255);
+ellipse(300,300,200,200);
+fill(0);
+ellipse(200,400,120,120);
 	}
 
      
