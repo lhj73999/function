5c8e922b718c1ea51b82da6a194602028c745a21
lhj73999 <ehgkfdl@naver.com>
2014-10-16 16:14:08 +0900

    fff

--- sketch.js
+++ sketch.js
@@ -10,19 +10,19 @@ function draw()
 	fill(255);
 	ellipse(mouseX, mouseY, 100, 100);
 
-drawdonut(100, 100);
-drawdonut(300, 100);
-drawdonut(400, 200);
-drawdonut(500, 300);
+drawdonut(255, 0, 0, 100, 100);
+drawdonut(255, 94, 0, 300, 100);
+drawdonut(255, 187, 0, 400, 200);
+drawdonut(255, 228, 0, 500, 300);
 
 }
 
-function drawdonut(x, y)
+function drawdonut(R, G, B, x, y)
 
 { 
 //doughnut #1
 
-fill(255);
+fill(R, G, B, 255);
 ellipse(x, y, 200, 200);
 fill(0);
 ellipse(x, y, 120, 120);
